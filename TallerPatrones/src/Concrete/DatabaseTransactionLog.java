4a9a392a5b946de4466f4d053697052eb7d9f6f9
rgomezj <rogergomez780@gmail.com>
2014-03-02 14:38:50 -0500

    Se agrega la clase DatabaseTransactionLog

---
+++ TallerPatrones/src/Concrete/DatabaseTransactionLog.java
@@ -0,0 +1,14 @@
+package Concrete;
+
+import Entities.CreditCard;
+import Entities.PizzaOrder;
+
+public class DatabaseTransactionLog implements Abstract.TransactionLog {
+
+	@Override
+	public void logOrder(CreditCard card, PizzaOrder order) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
