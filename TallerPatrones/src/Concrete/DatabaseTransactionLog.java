47f3d7ec39a26cd4c826034bd2637e5c59e11542
rgomezj <rogergomez780@gmail.com>
2014-03-02 18:02:04 -0500

    Se corrigen errores en dependencias y se prueba el Main

--- TallerPatrones/src/Concrete/DatabaseTransactionLog.java
+++ TallerPatrones/src/Concrete/DatabaseTransactionLog.java
@@ -2,8 +2,6 @@ package Concrete;
 
 import Abstract.UnreachableException;
 import Entities.ChargeResult;
-import Entities.CreditCard;
-import Entities.PizzaOrder;
 
 public class DatabaseTransactionLog implements Abstract.TransactionLog {
 
